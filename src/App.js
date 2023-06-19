130fdffbfb720ea755ba7263c60bbdb1530e5c8e
mitesh <miteshduiux@gmail.com>
2023-06-19 17:40:25 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import LoadingScreen from "./pages/LoadingScreen";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import Lake from "./pages/Lake";
+import Image3 from "./pages/Image3";
+import Mhl from "./pages/Mhl";
+import Header from "./components/Header";
+import Portfolio from "./pages/Portfolio";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/aboutus" element={<AboutUs />}></Route>
+            <Route path="/contactus" element={<ContactUs />}></Route>
+            <Route path="/portfolio" element={<Portfolio />}></Route>
+            <Route path="/lake" element={<Lake />}></Route>
+            <Route path="/alessandro" element={<Image3 />}></Route>
+            <Route path="/mhl" element={<Mhl />}></Route>
+          </Routes>
+        </Router>
+      </div>
+    </>
   );
 }
 
